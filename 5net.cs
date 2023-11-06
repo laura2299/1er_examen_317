579655448995624bc08524135e05d05e47f0000a
laura2299 <112607589+laura2299@users.noreply.github.com>
2023-11-06 09:42:40 +0400

    Update 5net.cs serie

--- 5net.cs
+++ 5net.cs
@@ -1,79 +1,53 @@
 using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-//ejercicio 5 seerie con .net
+using System.Threading;
+
 class Program
 {
-    static List<int> GenerateSeries(int n)
-    {
-        List<int> series = new List<int>();
-
-        if (n == 1)
-        {
-            series.Add(2);
-        }
-        else if (n == 0)
-        {
-            series.Add(0);
-        }
-        else if (n % 2 == 0)
-        {
-            int numThreads = n / 2;
-            int maxThreads = Math.Min(4, numThreads);
-
-            Parallel.For(1, maxThreads + 1, i =>
-            {
-                int index = i;
-                series.Add(index * index + 1);
-                series.Add(index + index);
-            });
-
-            for (int i = maxThreads + 1; i <= numThreads; i++)
-            {
-                int index = i;
-                lock (series)
-                {
-                    series.Add(index * index + 1);
-                    series.Add(index + index);
-                }
-            }
-        }
-        else
-        {
-            int numThreads = n / 2 +1;
-            int maxThreads = Math.Min(4, numThreads);
-
-            Parallel.For(1, maxThreads, i =>
-            {
-                int index = i;
-                series.Add(index * index + 1);
-                series.Add(index + index);
-            });
-
-            for (int i = maxThreads + 1; i <= numThreads; i++)
-            {
-                int index = i;
-                lock (series)
-                {
-                    series.Add(index * index + 1);
-                    series.Add(index + index);
-                }
-            }
-
-            series.Add(numThreads * numThreads + 1);
-        }
-
-        return series;
-    }
+    static int n = 10;
+    static int[] serie = new int[n];
+    static ManualResetEvent[] waitHandles = new ManualResetEvent[n]; // Arreglo de ManualResetEvent para sincronización
+    static object lockObject = new object(); // Objeto de bloqueo para garantizar la seguridad de subprocesos
+    static int currentIndex = 0; // Índice actual para el procesamiento en hilos
 
     static void Main()
     {
-        int n = 5;
-        List<int> resultingSeries = GenerateSeries(n);
-
-        foreach (int number in resultingSeries)
+        // Inicializa los ManualResetEvent para la sincronización
+        for (int i = 0; i < n; i++)
         {
-            Console.WriteLine(number);
+            waitHandles[i] = new ManualResetEvent(false);
+        }
+
+        // Inicia 4 hilos para generar los valores de la serie
+        for (int i = 0; i < 4; i++)
+        {
+            ThreadPool.QueueUserWorkItem(GenerateSeriesValues);
+        }
+
+        // Espera a que todas las tareas en ThreadPool se completen
+        WaitHandle.WaitAll(waitHandles);
+
+        // Imprime los valores de la serie
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(serie[i]);
+        }
+    }
+
+    static void GenerateSeriesValues(object state)
+    {
+        int index;
+        while ((index = Interlocked.Increment(ref currentIndex) - 1) < n)
+        {
+            // Calcula los valores de la serie y almacénalos en el arreglo
+            if (index % 2 == 0)
+            {
+                serie[index] = (int)Math.Pow((index / 2 + 1), 2) + 1;
+            }
+            else
+            {
+                serie[index] = index + 1;
+            }
+            waitHandles[index].Set(); // Indica que este índice ha sido procesado
         }
     }
 }
