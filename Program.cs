71f55041c085dfb10bd128ca565b230cd05d1a0e
laura2299 <112607589+laura2299@users.noreply.github.com>
2023-11-06 09:20:21 +0400

    Update Program.cs 7 pi

--- Program.cs
+++ Program.cs
@@ -1,70 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
+using System;
 using System.Threading;
 
 class Program
 {
-	
+    // Función para calcular la parte de PI en el rango asignado a un procesador específico
+    static double CalculoPi(int procesador, int limite, int procesadores)
+    {
+        double cpi = 0;
+        for (int i = procesador; i < limite; i += procesadores)
+        {
+            // Calcular pi utilizando la fórmula matemática
+            if (i != 0)
+            {
+                cpi += 1.0 / Math.Pow(i, 4);
+            }
+        }
+        return cpi;
+    }
 
-	static double CalculoPi(object estado)
-	{
-    	var parametros = (Tuple<int, int, int>)estado;
-    	int procesador = parametros.Item1;
-    	int limite = parametros.Item2;
-    	int procesadores = parametros.Item3;
+    // Función que realiza el cálculo de PI y establece un evento al finalizar
+    static void CalcularPiYSetEvent(int indice, int limite, int procesadores, double[] resultados, AutoResetEvent[] eventos)
+    {
+        // Llama a la función CalculoPi para calcular la parte de PI y almacénala en el arreglo de resultados
+        resultados[indice] = CalculoPi(indice, limite, procesadores);
+        // Establece el evento correspondiente para indicar que el cálculo ha terminado
+        eventos[indice].Set();
+    }
 
-    	double cpi = 0;
-    	for (int i = procesador; i < limite; i += procesadores)
-    	{
-        	// Calcular pi
-        	if (i != 0)
-        	{
-            	cpi += 1.0 / Math.Pow(i, 4);
-        	}
-    	}
-    	return cpi;
-	}
+    static void Main()
+    {
+        int limite = 1000000; // Establece el límite de cálculo
+        int procesadores = 4; // Utilizamos solo 4 hilos
 
-	static void Main()
-	{
-    	int limite = 1000000000;
-    	int procesadores = Environment.ProcessorCount;
-    	List<int> entradas = new List<int>();
-    	for (int i = 0; i < procesadores; i++)
-    	{
-        	entradas.Add(i);
-    	}
+        double[] resultados = new double[procesadores]; // Arreglo para almacenar los resultados parciales
+        AutoResetEvent[] eventos = new AutoResetEvent[procesadores]; // Arreglo de eventos para sincronización
 
-    	//Console.WriteLine(string.Join(", ", entradas));
+        for (int i = 0; i < procesadores; i++)
+        {
+            int indice = i; // Almacena el índice localmente para usarlo en el hilo
+            eventos[i] = new AutoResetEvent(false); // Inicializa un evento para el hilo actual
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                // Llama a la función para calcular PI y establecer el evento correspondiente
+                CalcularPiYSetEvent(indice, limite, procesadores, resultados, eventos);
+            });
+        }
 
-    	double[] resultados = new double[procesadores];
-    	AutoResetEvent[] eventos = new AutoResetEvent[procesadores];
+        WaitHandle.WaitAll(eventos); // Espera a que todos los eventos se activen
 
-    	for (int i = 0; i < procesadores; i++)
-    	{   
-        	eventos[i] = new AutoResetEvent(false);
-        	ThreadPool.QueueUserWorkItem(state =>
-        	{
-                int indice;
-                if (state is not null){
-                    indice = (int)state;
-                
-            	
-            	resultados[indice] = CalculoPi(Tuple.Create(indice, limite, procesadores));
-            	eventos[indice].Set();
-                }
-            }, i);
-    	}
+        double sumaResultado4 = 0;
+        foreach (var resultado in resultados)
+        {
+            sumaResultado4 += resultado; // Suma los resultados parciales
+        }
 
-    	WaitHandle.WaitAll(eventos);
-
-    	double sumaResultado4 = 0;
-    	foreach (var resultado in resultados)
-    	{
-        	sumaResultado4 += resultado;
-    	}
-
-    	double resultadoFinal = Math.Pow(sumaResultado4 * 90, 1.0 / 4);
-    	Console.WriteLine($"Resultado final: {resultadoFinal}");
-	}
+        double resultadoFinal = Math.Pow(sumaResultado4 * 90, 1.0 / 4); // Calcula el resultado final de PI
+        Console.WriteLine($"Resultado final: {resultadoFinal}");
+    }
 }
